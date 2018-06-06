564139a85b9bc94d099941f3106e84c2a49a6365
jiro.kirameki <ec2-user@ip-172-31-44-188.us-east-2.compute.internal>
2018-06-06 05:21:26 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,19 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-
+        <div class="col-xs-12">
+        <div class="col-sm-8 col-sm-offset-2">
+        <div class="col-md-8 col-md-offset-2">
+        <div class="col-lg-6 col-lg-offset-3">
     <h1>tasklists</h1>
-
+ <div class="row">
+       
+            
     {!! Form::model($tasks, ['route' => 'tasks.store']) !!}
     
+     <div class="form-group">
      {!! Form::label('content', 'task:') !!}
         {!! Form::text('content') !!}
+ </div>
 
+<div class="form-group">
       {!! Form::label('status', 'status:') !!}
         {!! Form::text('status') !!}
+ </div>
 
-        {!! Form::submit('post') !!}
+        {!! Form::submit('post', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
-
+ </div>
+ </div>
+ </div>
+ </div> 
+ </div>
 @endsection
+
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,13 +4,28 @@
 
     <h1>updated tasks</h1>
 
-    <p>task :    {{ $tasks->content }}</p>
-     <p>status :    {{ $tasks->status }}</p>
+   <table class="table table-bordered">
+     
+        <tr>
+            <th>id</th>
+            <td>{{ $tasks->id }}</td>
+        </tr>
+        <tr>
+            <th>タスク</th>
+            <td>{{ $tasks->content }}</td>
+        </tr>
+        <tr>
+            <th>ステータス</th>
+            <td>{{ $tasks->status }}</td>
+        </tr>
+     
+       
+    </table>
 
-    {!! link_to_route('tasks.edit', 'Edit this task', ['id' => $tasks->id]) !!}
+    {!! link_to_route('tasks.edit', 'Edit this task', ['id' => $tasks->id], ['class' => 'btn btn-default']) !!}
 
     {!! Form::model($tasks, ['route' => ['tasks.destroy', $tasks->id], 'method' => 'delete']) !!}
-        {!! Form::submit('Delete this task') !!}
+        {!! Form::submit('delete', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
 
 @endsection
