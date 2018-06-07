c3bd24450bda60e9e59ecb9b5df7527533f8787e
jiro.kirameki <ec2-user@ip-172-31-44-188.us-east-2.compute.internal>
2018-06-07 03:50:59 +0000

    Procfile

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,31 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="col-xs-12">
-        <div class="col-sm-8 col-sm-offset-2">
-        <div class="col-md-8 col-md-offset-2">
-        <div class="col-lg-6 col-lg-offset-3">
+       <div class = "container">
+         <div class="row">
+           <div class="col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+       
     <h1>tasklists</h1>
- <div class="row">
+
        
             
     {!! Form::model($tasks, ['route' => 'tasks.store']) !!}
     
      <div class="form-group">
      {!! Form::label('content', 'task:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
  </div>
 
 <div class="form-group">
       {!! Form::label('status', 'status:') !!}
-        {!! Form::text('status') !!}
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
  </div>
 
         {!! Form::submit('post', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
- </div>
- </div>
  </div>
  </div> 
  </div>
