502cbae12e04626da0065bb56922b3dc1b0068af
jiro.kirameki <ec2-user@ip-172-31-44-188.us-east-2.compute.internal>
2018-06-08 07:39:02 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ Route::get('/', 'tasksController@index');
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
